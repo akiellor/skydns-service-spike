7a9d218ef20fc99632849ff13aa668ae1a02afaa
Andrew Kiellor <akiellor@gmail.com>
2014-03-04 04:39:16 -0800

    Actually do a check in the health check app.

--- health/src/main/java/services/doctor/Doctor.java
+++ health/src/main/java/services/doctor/Doctor.java
@@ -3,6 +3,9 @@ package services.doctor;
 import services.Health;
 import services.registry.ServiceRegistry;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,8 +13,33 @@ public class Doctor {
     public EnvironmentHealth check(ServiceRegistry.Environment environment) {
         List<Health> health = new ArrayList<Health>();
         for (ServiceRegistry.Service service : environment.getServices()) {
-            health.add(new Health(service.getDomain(), service.getPort(), true));
+            int port = service.getPort();
+            String domain = service.getDomain();
+
+            boolean ok = check(domain, port);
+
+            health.add(new Health(domain, port, ok));
         }
         return new EnvironmentHealth(environment.getEnvironment(), health);
+    }
+
+    private boolean check(String domain, int port) {
+        Socket socket = new Socket();
+        boolean ok = true;
+        try {
+            socket.connect(new InetSocketAddress(domain, port));
+            ok = socket.isConnected();
+        } catch (IOException e) {
+            ok = false;
+        } finally {
+            if(socket.isConnected()) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    ok = false;
+                }
+            }
+        }
+        return ok;
     }
 }
